e6598707ee022e7944687c4913e1162e1a9ff4c4
Vahid <vahid.bozkurt1994@gmail.com>
2025-07-27 18:34:30 +0330

    Updating globals.css and tailwind.config again

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container"></section>
+
+      <h1 className="heading">Home</h1>
     </>
   );
 }
